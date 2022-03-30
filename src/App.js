098408ab0177360972ae286292096d26654076d0
Ken <kenblackuoo@gmail.com>
2022-03-30 16:14:25 -0400

    Updated DisplayGames component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import React from 'react';
 import DisplayGames from './Components/DisplayGames/DisplayGames';
 
 function App() {
 
-  const [allGames, setAllGames] = useState([]);  
+  const [allGames, setAllGames] = useState([]); 
 
+  useEffect(() => { //this is used to render the list of songs on App execution
+    getAllGames();
+  }, [])
 
   async function getAllGames(){
     let response = await axios.get('https://localhost:7260/api/games');
     setAllGames(response.data);
+    console.log(response.data)
   }
 
   async function getGameById(game){
@@ -23,7 +26,7 @@ function App() {
 
   return (
     <div>
-      <DisplayGames DisplayGames = {games}/>
+      <DisplayGames displayGames = {allGames}/>
     </div>
   );
 }
--- src/Components/DisplayGames/DisplayGames.jsx
+++ src/Components/DisplayGames/DisplayGames.jsx
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
 import './DisplayGames.css';
 
-const DisplayGames = (props) => {
-    return (  
-        <div>
 
+const DisplayGames = (props) => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    return (  
+        <div className = 'display-container'>
+        <div className='table-title'>
+            <div className ='library-contain'>
+                <h2 className='library-title'>List of Games</h2>
+            </div>
+            <div className = 'search-filter'>
+                <label className='search-label'>Filter Game List:</label>
+                <input type='text' className='custom-input' placeholder="Search...(e.g. Nintendo)" onChange={(event) => setSearchTerm(event.target.value)}/>
+            </div>
         </div>
+        <table className='song-table table'>
+        <thead>
+            <tr className = 'header-row'>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Platform</th>            
+                <th>Year</th>
+                <th>Genre</th>
+                <th>Publisher</th>
+            </tr>
+        </thead>
+        <tbody>
+            {props.displayGames.filter((game) =>{
+                if (searchTerm == ""){
+                    return game;
+                }
+                else if (game.rank.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || game.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || game.platform.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || game.year.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || game.genre.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || game.publisher.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                    return game;
+                }
+            })
+            .map((game, index) => { 
+            return (
+                <tr key= {index} className = 'display-rows'>
+                    <td>{game.rank}</td>
+                    <td>{game.name}</td>
+                    <td>{game.platform}</td>
+                    <td>{game.year}</td>
+                    <td>{game.genre}</td>
+                    <td>{game.publisher}</td>
+                </tr>
+            )
+            })}
+        </tbody>
+        </table>
+    </div>  
     );
 }
  
