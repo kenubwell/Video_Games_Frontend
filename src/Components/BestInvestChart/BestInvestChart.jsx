658df85139b33bbf6c02c8f2a16d47a986ca23d3
Sydney Wilson <syd.toon17@gmail.com>
2022-03-31 16:46:50 -0500

    applied chart and filter to 2013 games data

--- src/Components/BestInvestChart/BestInvestChart.jsx
+++ src/Components/BestInvestChart/BestInvestChart.jsx
@@ -5,29 +5,55 @@ import { Chart } from "react-google-charts";
 const BestInvestChart = (props) => {
 
     const [chartData, setChartData] =useState([]);
+    const [platformData, setPlatformData] = useState([]);
 
     useEffect(() =>{
         let tempChartData = props.allGames.filter(function(element){
             return element.year >= 2013;
-        }).map(entry => {
-        return [entry.platform, entry.globalSales];
-    });
-    setChartData(tempChartData);
+        })
+    identifyPlatformInfo(tempChartData);
 }, [props.allGames])
 console.log(chartData)
 
-    return (  
-        <div className='console-chart-contain'>
-        <Chart
-        chartType="Bar"
-        data={[["Console", "Sales"], ...chartData]}
-        width="100%"
-        height="350px"
-        options={{legend: {position: 'bottom'}}}
-        legendToggle
-        />
+
+    function identifyPlatformInfo(tempChartData){
+        console.log(tempChartData);
+        const duplicatePlatform = tempChartData.map(game => game.platform);
+        console.log(duplicatePlatform);
+
+        const distinctPlatform = [...new Set(duplicatePlatform)];
+
+        let platformsWithGlobalSales = distinctPlatform.map(platformName => {
+            let platformDataObject ={
+                platform : platformName,
+
+                gamesSoldGlobally: tempChartData.filter(game => game.platform === platformName).map(game => game.globalSales).reduce((a, b) => a + b, 0)
+            }
+            return platformDataObject;
+        })
+
+        setPlatformData(platformsWithGlobalSales);
+    }
+
+    function formatPlatformData(platformData){
+        const data = [
+            ["Platform", "Global Sales", {role: "style"}],
+            ...platformData.map(platformDataSingle => [platformDataSingle.platform, platformDataSingle.gamesSoldGlobally, 'lightblue'])
+        ]
+    return data
+    }
+
+
+return (  
+    <div>
+        {platformData.length > 0 &&
+        <>
+            <h1>Platform By Global Sales in Millions</h1>
+            <Chart chartType="ColumnChart" width="100%" height="400px" data={formatPlatformData(platformData)} />
+        </>
+        }
     </div>
-    );
+);
 }
  
 export default BestInvestChart;
